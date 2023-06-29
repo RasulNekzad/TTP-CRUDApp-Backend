eeee252bb4efcf238eb811842d57e2fa647c1ed2
Rasul Nekzad <nekzadrasul@gmail.com>
2023-06-28 22:14:28 -0400

    wrote route to serve student based on id and associated campus

--- api/student.js
+++ api/student.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Student } = require("../db/models");
+const { Campus, Student } = require("../db/models");
 
 // root is localhost:3000/api/student
 router.get("/", async (req, res, next) => {
@@ -11,6 +11,26 @@ router.get("/", async (req, res, next) => {
       : res.status(404).send("Student List Not Found");
   } catch (error) {
     next(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const studentId = req.params.id;
+    const student = await Student.findByPk(studentId);
+    const campus = await Campus.findByPk(student.CampusId);
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.status(200).json({
+      student: student,
+      campus: campus,
+    });
+  } catch (error) {
+    console.error("Error retrieving student:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
