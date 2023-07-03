fe73d9004f65eae95725f353f0410390edc66753
Rasul Nekzad <nekzadrasul@gmail.com>
2023-07-03 12:12:17 -0400

    #1 - Added pg as dependency to sequelize ORM connection

--- db/db.js
+++ db/db.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
-const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require");
+const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
+  dialectModule: pg,
+});
 
 module.exports = db;
