3040eef891045baeee00d240a92cbcf2abfdd7d5
Rasul Nekzad <nekzadrasul@gmail.com>
2023-07-03 13:06:15 -0400

    Sync DB force set to false.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 
 app.use(cors());
 app.use("/api", require("./api"));
-const syncDB = () => db.sync({ force: true });
+const syncDB = () => db.sync({ force: false });
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
